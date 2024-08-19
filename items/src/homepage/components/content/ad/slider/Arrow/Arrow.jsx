cf4fde2a76c39b53e47da95b6c80871290808942
Alex <fornikaras7@gmail.com>
2024-08-19 15:25:17 +0500

    feat: add slider animation

--- items/src/homepage/components/content/ad/slider/Arrow/Arrow.jsx
+++ items/src/homepage/components/content/ad/slider/Arrow/Arrow.jsx
@@ -1,7 +1,7 @@
 import "./Arrow.scss";
-export const Arrow = ({ className, children }) => {
+export const Arrow = ({ className, children, onClick }) => {
   return (
-    <div className={`arrow ${className}`}>
+    <div className={`arrow ${className}`} onClick={onClick}>
       <div className="arrow__icon">{children}</div>
     </div>
   );
--- items/src/homepage/components/content/ad/slider/Slider.jsx
+++ items/src/homepage/components/content/ad/slider/Slider.jsx
@@ -1,19 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Arrow } from "./Arrow/Arrow.jsx";
 
 import "./Slider.scss";
 
 export const Slider = () => {
-  const [slideNumber, setSlideNumber] = useState(1);
+  const [slideNumber, setSlideNumber] = useState(0);
+
+  const slides = ["first", "second"];
+
+  const handleNext = () => {
+    setSlideNumber((prev) => (prev + 1) % slides.length);
+  };
+
+  const handlePrev = () => {
+    setSlideNumber((prev) => (prev - 1 + slides.length) % slides.length);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(handleNext, 3000);
+    return () => clearInterval(interval);
+  }, [slideNumber]);
+
   return (
     <section className="adsection">
       <div className="slider">
-        <Arrow className="arrow__left"></Arrow>
-        <Arrow className="arrow__right"></Arrow>
+        <Arrow className="arrow__left" onClick={handlePrev}>
+          
+        </Arrow>
+        <Arrow className="arrow__right" onClick={handleNext}>
+          
+        </Arrow>
         <div className="slider__mask">
-          <div className="slide__item first"></div>
-          <div className="slide__item second"></div>
+          {slides.map((slide, index) => (
+            <div
+              key={index}
+              className={`slide__item ${slide} ${
+                index === slideNumber ? "active" : ""
+              }`}
+            ></div>
+          ))}
         </div>
       </div>
     </section>
