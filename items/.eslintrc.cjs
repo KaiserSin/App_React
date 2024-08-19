0336c5428ea1f113bf3e54b717528e7a00d8e418
mikhail <michael.maidan1998@gmail.com>
2024-08-19 11:54:50 +0200

    eslint was finally done

--- items/.eslintrc.cjs
+++ items/.eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     'react/jsx-no-target-blank': 'off',
     "react/prop-types": "off",
+    "no-unused-vars": "off",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
