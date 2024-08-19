6f9cc9d6d09dca6d997fd84ca9e2a27efd70d6ae
mikhail <michael.maidan1998@gmail.com>
2024-08-19 10:34:04 +0200

    Vite.config set up

--- items/vite.config.js
+++ items/vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+
+const resolveAlias = (p) => resolve(__dirname, p);
+const alias = { 
+  "@": resolveAlias('./src'),
+  "~": resolveAlias('./'),
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080,
+  },
+  build:{
+    target: "esnext",
+
+  },
+  resolve: {
+    alias,
+  }
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
