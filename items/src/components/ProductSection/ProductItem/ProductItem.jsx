115403dd4c48c86ebccb2e3a61fa27d3afcbb757
mikhail <michael.maidan1998@gmail.com>
2024-08-20 09:30:11 +0200

    more hierarchies, footer and styling

--- items/src/components/ProductSection/ProductItem/ProductItem.jsx
+++ items/src/components/ProductSection/ProductItem/ProductItem.jsx
@@ -4,11 +4,13 @@ import { Card } from "../ProductCard/ProductCard"
 import { Description } from "../ProductDescription/ProductDescription"
 import {v4 as uuidv4} from 'uuid'
 
-const Item = ({id, image_url, currentprice, oldprice, text}) =>{
-    return <div id = {id} className="item" >
-        <Card image_url={image_url}/>
-        <Description currentprice={currentprice} oldprice={oldprice} text={text}/>
-    </div>
-}
+const Item = ({id, image_url, currentprice, oldprice, text}) => {
+    return (
+        <div id = {id} className="item" >
+            <Card image_url={image_url}/>
+            <Description currentprice={currentprice} oldprice={oldprice} text={text}/>
+        </div>
+    );
+    }
 
 export {Item}
--- items/src/components/footer/Footer.jsx
+++ items/src/components/footer/Footer.jsx
@@ -1,21 +1,36 @@
-import LText from "./LText"
-import Support from "./Support"
+import './Footer.scss'
 
-
-const Footer = () =>{
-    return(
-        <footer id="footer" className="footerblock">
-            <div className="footerbg"></div>
-            <div className="footer">
-                <LText/>
-                <Support/>
-                <a href="#" className="link-block-29 w-inline-block">
-                    <img src="https://cdn.prod.website-files.com/66ab7b46556019f9117db3ca/66ac2a52450e2ad9e7051e04_Frame%2064.png" 
-                    loading="lazy" width="77" alt="" className="image-20"/>
-                </a>
+const Footer = () => {
+    return (
+      <footer id="footer" className="footerblock">
+        <div className="footerbg"></div>
+        <div className="footer">
+          <div className="text-block">
+            2023-2024 ©SwiftMart — онлайн маркетплейс. Все права защищены. Доставка по всей России и странам СНГ. На Торговой площадке SwiftMart применяются рекомендательные технологии. Адрес для направления юридически значимых сообщений: 
+            <a href="mailto:legal@swiftmart.ru" className="link">legal@swiftmart.ru</a>
+          </div>
+          <div className="support-block">
+            <div className="text-block-2">Поддержка</div>
+            <div className="text-block-3">
+              Горячая линия: <a href="tel:+7(800)222-22-22" className="link-2">+7 (800) 222-22-22</a>
             </div>
-        </footer>
+            <div className="text-block-5">
+              Email: <a href="mailto:support@swiftmart.ru" className="link-3">support@swiftmart.ru</a>
+            </div>
+          </div>
+          <a href="#" className="link-block-29 w-inline-block">
+            <img
+              src="https://cdn.prod.website-files.com/66ab7b46556019f9117db3ca/66ac2a52450e2ad9e7051e04_Frame%2064.png"
+              loading="lazy"
+              width="77"
+              alt=""
+              className="qr-code"
+            />
+          </a>
+        </div>
+      </footer>
     );
-}
-
-export default Footer
+  };
+  
+  export default Footer;
+  
