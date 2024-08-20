430af3f130683c43f733e4d8a13e55ed374558cc
mikhail <michael.maidan1998@gmail.com>
2024-08-20 12:18:08 +0200

    Some changes for LevelBlock

--- items/src/components/ProgressBar/ProgressBar.jsx
+++ items/src/components/ProgressBar/ProgressBar.jsx
@@ -1,17 +1,17 @@
-
 import './ProgressBar.scss';
 
 const ProgressBar = ({ progress }) => {
     document.documentElement.style.setProperty("--pers", `${progress/50 *100}%`);
     return (
-      <div className="w-layout-grid progressgrid">
-        <div  className="level grid-level">Ваш уровень</div>
-        <div className="progress-container grid-level2">
+      <div className="progressgrid">
+        <div className="level">Ваш уровень</div>
+        <div className="progress-container">
           <div className="progress-bar"></div>
         </div>
         <div className="leveld">До следующего уровня: {progress}/50 покупок</div>
       </div>
     );
-  };
+};
 
-export {ProgressBar}
+export { ProgressBar }
+
