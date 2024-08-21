ec58b8ed76cf97877e90024d047c84d9d3853267
ce-fello <7552494@gmail.com>
2024-08-21 04:17:19 -0400

    Created Catalog component, Menu component and Category

---
+++ items/src/components/Catalog/Catalog.jsx
@@ -0,0 +1,12 @@
+import Menu from "./Menu";
+import './Catalog.scss'
+
+function Catalog() {
+    return(
+        <section className="catalogpc">
+            <Menu/>
+        </section>
+    )
+}
+
+export default Catalog;
