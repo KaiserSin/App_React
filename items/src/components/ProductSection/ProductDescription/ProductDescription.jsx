415acfc67b81546384c9ae43a8fb43677964cd79
mikhail <michael.maidan1998@gmail.com>
2024-08-21 11:38:37 +0200

    Successful Content ProductCard rendering was done!

--- items/src/components/ProductSection/ProductDescription/ProductDescription.jsx
+++ items/src/components/ProductSection/ProductDescription/ProductDescription.jsx
@@ -5,9 +5,9 @@ const Description = ({ currentPrice, oldPrice, text }) => {
     return (
         <div className="description">
             <a href="#" className="description__price">
-                <div className="description__price-current">{currentPrice}</div>
+                <div className="description__price-current">{currentPrice}₽</div>
                 <div className="description__price-old">
-                    <sup>{oldPrice}</sup>
+                    <sup>{oldPrice}₽</sup>
                 </div>
             </a>
             <a href="#" className="description__text">
--- items/src/components/ProductSection/ProductItem/ProductItem.jsx
+++ items/src/components/ProductSection/ProductItem/ProductItem.jsx
@@ -7,8 +7,9 @@ import {v4 as uuidv4} from 'uuid'
 const Item = ({id, image_url, currentprice, oldprice, text}) => {
     return (
         <div id = {id} className="item" >
-            <Card image_url={image_url}/>
-            <Description currentprice={currentprice} oldprice={oldprice} text={text}/>
+            <Card imageUrl={image_url} />
+            <Description currentPrice={currentprice} oldPrice={oldprice} text={text} />
+
         </div>
     );
     }
