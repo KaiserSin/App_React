29ab74e0b5f9d05763ef5569981e6af795c2c581
Alex <fornikaras7@gmail.com>
2024-08-22 20:26:16 +0500

    feat: add route for profile

--- items/src/routes/index.jsx
+++ items/src/routes/index.jsx
@@ -1,7 +1,11 @@
+import { Profile } from "../components/Profile/Profile";
 import HomePage from "../pages/Home/HomePage";
 
 export const privateRoutes = [
   // {path: "/", element:  }
 ];
 
-export const publicRoutes = [{ path: "/", element: <HomePage /> }];
+export const publicRoutes = [
+  { path: "/", element: <HomePage /> },
+  { path: "/profile", element: <Profile /> },
+];
