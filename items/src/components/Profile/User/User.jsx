a6700c7a4b3849ff2a6b32f99aaa8d6b4239360b
Alex <fornikaras7@gmail.com>
2024-08-22 17:33:34 +0500

    feat: add component for user security

--- items/src/components/Profile/User/User.jsx
+++ items/src/components/Profile/User/User.jsx
@@ -1,6 +1,7 @@
 import { userInfo } from "../Constants/User-info";
 
 import "./User.scss";
+import { UserSecurity } from "./UserSecurity/User-security";
 
 export const User = () => {
   return (
@@ -50,48 +51,7 @@ export const User = () => {
             </div>
           </div>
         </div>
-        <div className="user__safety">
-          <div className="user__info-title">
-            <h2>Безопасность </h2>
-            <p>
-              Управляйте настройками двухфакторной <br />
-              аутентификации, активными сессиями и паролем.
-            </p>
-          </div>
-          <div className="bottom__blocks">
-            <div className="botttom__block">
-              <div className="content__block">
-                <div className="password">
-                  <img src="/profile-images/shieldIcon.png" alt="" />
-                  <span>Пароль</span>
-                </div>
-                <div className="safe-password">Надежный пароль</div>
-              </div>
-              <button className="check__botton">Проверка на утечки</button>
-            </div>
-            <button className="change__button">Изменить</button>
-            <div>
-              <div className="botttom__block">
-                <div className="content__block">
-                  <div>
-                    <div className="password">
-                      <img src="/profile-images/shieldsIcon.png" alt="" />
-                      <span>2FA Защита</span>
-                    </div>
-                  </div>
-                  <div className="sms__block">
-                    <span>СМС Сообщение</span>
-                    <img src="/profile-images/toBottomIcon.png" alt="" />
-                  </div>
-                </div>
-                <button className="delete__account-button">
-                  Удалить аккаунт
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="buttons__block"></div>
-        </div>
+        <UserSecurity />
       </div>
     </div>
   );
