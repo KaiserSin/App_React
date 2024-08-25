22634f4681333d44f1a2ad3b0e22dba0e67c7013
KaiserSin <arten.smernov@bk.ru>
2024-08-26 00:08:59 +0300

    some fixes with server

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.get('*', (req, res) => {
 
 // Указываем порт для прослушивания
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
