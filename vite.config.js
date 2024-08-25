f959a7d4a6401895fd56e06e511c4bf9c6c4a6fe
Alex <fornikaras7@gmail.com>
2024-08-25 22:26:44 +0500

    fix: add vite config

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+const resolveAlias = (p) => resolve(__dirname, p);
+const alias = {
+  "@": resolveAlias("./src"),
+  "@assets": resolveAlias("./src/assets"),
+  "@shared": resolveAlias("./src/shared"),
+  "@features": resolveAlias("./src/features"),
+  "@core": resolveAlias("./src/core"),
+  "~": resolveAlias("./"),
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: "esnext",
+  },
+  resolve: {
+    alias,
+  },
+});
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
