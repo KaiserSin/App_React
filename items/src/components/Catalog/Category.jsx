40fa5228523979cf4cc64228f499fef3f3bb65ba
ce-fello <7552494@gmail.com>
2024-08-25 15:25:09 +0300

    starting to prepare categories component to be able to be chosen

--- items/src/components/Catalog/Category.jsx
+++ items/src/components/Catalog/Category.jsx
@@ -1,14 +1,23 @@
+import { useState } from 'react'
 import './Catalog.scss'
 
-export function Category ({id, name, imageUrl}){
-    
+export function Category ({id, name, imageUrl, chosen, onClick}){
+    const [style, setStyle] = useState("list-item-idle")
+
     const handleClickCategory = () => {
-        console.log('clicked category ' + name)
+        console.log('Clicked ' + name + ' category')
+        if (chosen == "true") {
+            setStyle("list-item")
+            console.log('Set ' + name + ' category style to list-item')
+        } else {
+            setStyle("list-item-idle")
+            console.log('Set ' + name + ' category style to list-item-idle')
+        }
     }
 
     return(
-        <li id={id} className="list-item-idle">
-            <a className="link-block-31" href="#" onClick={handleClickCategory}>
+        <li id={id} className={style} onClick={onClick}>
+            <a className="link-block-31" href="#">
                 <img src={imageUrl} loading="lazy" alt="" width="25"/>
                 <div className="text-block-19">
                     {name}
--- items/src/components/Catalog/Menu.jsx
+++ items/src/components/Catalog/Menu.jsx
@@ -3,10 +3,21 @@ import { categories } from "../../shared/composables/constants/constant.js";
 import "./Catalog.scss"
 
 export function Menu() {
+    const onClick = () => {
+        console.log('Clicked category')
+        if (chosen == "true") {
+            setStyle("list-item")
+            console.log('Set ' + name + ' category style to list-item')
+        } else {
+            setStyle("list-item-idle")
+            console.log('Set ' + name + ' category style to list-item-idle')
+        }
+    }
+
     const catalogData = []
     categories.forEach(element => {
         catalogData.push(
-            <Category key={element.id} id={element.id} name={element.categoryName} imageUrl={element.imageUrl}/>
+            <Category key={element.id} id={element.id} name={element.categoryName} imageUrl={element.imageUrl} chosen={true} onClick={onClick}/>
         )
     })
     return(
