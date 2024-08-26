2d4bb11e319feda7d06e6ae71e910a7248b9c7ac
KaiserSin <arten.smernov@bk.ru>
2024-08-26 19:17:27 +0300

    Fixed mis server

--- server.js
+++ server.js
@@ -5,20 +5,20 @@ import { dirname as getDirname } from 'path';
 
 const app = express();
 
-// Определение текущей директории для работы с ES-модулями
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = getDirname(__filename);
 
-// Указываем директорию для статических файлов
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Обработка всех маршрутов, чтобы на них отвечал index.html
-app.get('*', (req, res) => {
+
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
-// Указываем порт для прослушивания
-const PORT = process.env.PORT || 5000;
+
+const PORT = 5172;
 app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
--- src/services/items.js
+++ src/services/items.js
@@ -1,22 +1,40 @@
 import axios from 'axios';
+import axiosRetry from 'axios-retry';
 
 const baseUrl = "http://31.129.54.67:8080";
+
+axiosRetry(axios, { 
+    retries: 10, 
+    retryDelay: (retryCount) => {
+        return retryCount * 1000; 
+    },
+    retryCondition: () => {
+        return true;
+    }
+});
 
 const getItems = (first, second) => {
     const params = {
         "from": first, 
         "to": second
-    }
+    };
     const queryString = new URLSearchParams(params).toString();
-    const request = axios.get(`${baseUrl}/getGoodsCompatByIdInRange?${queryString}` );
-    return request.then(response => response.data);
-}
-
+    return axios.get(`${baseUrl}/getGoodsCompatByIdInRange?${queryString}`)
+        .then(response => response.data)
+        .catch(error => {
+            console.error('Ошибка при получении элементов:', error);
+            throw error;
+        });
+};
 
 const getSize = () => {
-    const request = axios.get(`${baseUrl}/getGoodsCount`); 
-    return request.then(response => response.data);
-}
+    return axios.get(`${baseUrl}/getGoodsCount`)
+        .then(response => response.data)
+        .catch(error => {
+            console.error('Ошибка при получении размера:', error);
+            throw error;
+        });
+};
 
 export default {
     getItems,
