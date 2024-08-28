660415843b0b8d2acc955a28a7d341ce6baeb049
Alex <fornikaras7@gmail.com>
2024-08-28 19:25:46 +0500

    feat: add route to catalog

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,19 +1,16 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "@core/Header/components/Header/Header";
 import Footer from "@core/Footer/Footer";
 import Main from "@core/Main/Main";
-import { useSelector } from "react-redux";
-import { Catalog } from "@features/market/components/Catalog/Catalog";
 
 const MainLayout = () => {
-  const { isCatalogVisible } = useSelector((state) => {
-    return state.catalog;
-  });
   return (
     <>
       <Header />
-      <Main>{isCatalogVisible ? <Catalog /> : <Outlet />}</Main>
+      <Main>
+        {" "}
+        <Outlet />
+      </Main>
       <Footer />
     </>
   );
