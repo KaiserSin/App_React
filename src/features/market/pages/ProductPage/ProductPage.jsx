c32193f58c056f04ee69d75c6362f95313711552
itsDreizer <artem39r@mail.ru>
2024-08-30 12:50:25 +0200

    Product Component Refactoring and SimilarProductSection Component

--- src/features/market/pages/ProductPage/ProductPage.jsx
+++ src/features/market/pages/ProductPage/ProductPage.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { useGetProductQuery } from "@/redux/reducers/marketApi";
 
-import "./ProductPage.scss";
 import ProductGalleryBlock from "@features/market/components/ProductGalleryBlock/ProductGalleryBlock";
 import ProductInfoBlock from "@features/market/components/ProductInfoBlock/ProductInfoBlock";
 import ReviewsSection from "@features/market/components/ReviewsSection/ReviewsSection";
 import ProductCartBlock from "@features/market/components/ProductCartBlock/ProductCartBlock";
-
+import SimilarProductsSection from "@shared/components/SimilarProductsSection/SimilarProductsSection";
+import "./ProductPage.scss";
 const ProductPage = () => {
   const { data } = useGetProductQuery();
   console.log(data);
@@ -20,6 +20,7 @@ const ProductPage = () => {
           <ProductCartBlock />
         </div>
       </section>
+      <SimilarProductsSection />
       <ReviewsSection />
     </>
   );
