5a71c8b4edea5b46d10b189bf89ced8d4b1224ca
Alex <fornikaras7@gmail.com>
2024-09-05 13:51:49 +0500

    feat: fix mobile footer

---
+++ src/core/Constants/MobileFooterItems.js
@@ -0,0 +1,32 @@
+import HomeButton from "@assets/mobileFooterIcons/IconsSVG/HomeButton.svg";
+import AccountButton from "@assets/mobileFooterIcons/IconsSVG/AccountButton.svg";
+import AiButton from "@assets/mobileFooterIcons/IconsSVG/AiButton.svg";
+import CartButton from "@assets/mobileFooterIcons/IconsSVG/CartButton.svg";
+import Search from "@assets/mobileFooterIcons/IconsSVG/Search.svg";
+import Strips from "@assets/mobileFooterIcons/IconsSVG/Strips.svg";
+
+export const MobileFooterItems = [
+  {
+    iconSrc: HomeButton,
+    alt: "Home",
+    link: "/",
+  },
+  {
+    iconSrc: AccountButton,
+    alt: "User",
+    link: "/",
+  },
+  {
+    iconSrc: AiButton,
+    alt: "AI",
+    link: "/",
+  },
+  {
+    iconSrc: CartButton,
+    alt: "Cart",
+    notificationCount: 4,
+    link: "/cart",
+  },
+  { iconSrc: Search, alt: "Search" },
+  { iconSrc: Strips, alt: "Menu" },
+];
