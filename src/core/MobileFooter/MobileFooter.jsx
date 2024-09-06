54335ff4ee786539b7d97b2c922e5f9f0cb99f12
mikhail <michael.maidan1998@gmail.com>
2024-09-06 11:21:54 +0200

    refactor: catalogButton & cartButton in mobile version transfer to the mainPage now

--- src/core/MobileFooter/MobileFooter.jsx
+++ src/core/MobileFooter/MobileFooter.jsx
@@ -1,32 +1,82 @@
 import IconWithNotification from "@shared/components/IconWithNotification/IconWithNotification";
 import "./MobileFooter.scss";
 import { MobileFooterItems } from "../Constants/MobileFooterItems";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 export const MobileFooter = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleStripsClick = () => {
+    if (location.pathname === "/catalog") {
+      navigate("/");
+    } else {
+      navigate("/catalog");
+    }
+  };
+
+  const handleCartClick = () => {
+    if (location.pathname === "/cart") {
+      navigate("/");
+    } else {
+      navigate("/cart");
+    }
+  };
+
   return (
     <footer className="mobile-footer">
       <div className="mobile-footer__icons">
-        {MobileFooterItems.map((item, index) =>
-          item.notificationCount ? (
-            <Link key={index} to={item.link}>
-              <IconWithNotification
+        {MobileFooterItems.map((item, index) => {
+          if (item.alt === "Menu") {
+            return (
+              <button
                 key={index}
-                classes={{ image: "mobile-footer__icon" }}
-                src={item.iconSrc}
-                notificationCount={item.notificationCount}
-              />
-            </Link>
-          ) : (
-            <Link key={index} to={item.link}>
-              <img
-                src={item.iconSrc}
-                alt={item.iconSrc}
-                className="mobile-footer__icon"
-              />
-            </Link>
-          )
-        )}
+                onClick={handleStripsClick}
+                className="mobile-footer__icon-button"
+              >
+                <img
+                  src={item.iconSrc}
+                  alt={item.alt}
+                  className="mobile-footer__icon"
+                />
+              </button>
+            );
+          } else if (item.alt === "Cart") {
+            return (
+              <button
+                key={index}
+                onClick={handleCartClick}
+                className="mobile-footer__icon-button"
+              >
+                <IconWithNotification
+                  classes={{ image: "mobile-footer__icon" }}
+                  src={item.iconSrc}
+                  notificationCount={item.notificationCount}
+                />
+              </button>
+            );
+          } else if (item.notificationCount) {
+            return (
+              <Link key={index} to={item.link}>
+                <IconWithNotification
+                  classes={{ image: "mobile-footer__icon" }}
+                  src={item.iconSrc}
+                  notificationCount={item.notificationCount}
+                />
+              </Link>
+            );
+          } else {
+            return (
+              <Link key={index} to={item.link}>
+                <img
+                  src={item.iconSrc}
+                  alt={item.alt}
+                  className="mobile-footer__icon"
+                />
+              </Link>
+            );
+          }
+        })}
       </div>
     </footer>
   );
