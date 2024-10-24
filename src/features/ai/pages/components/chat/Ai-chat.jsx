e3152600b11024cc5d961f74c3a1a6d54b57397a
Alex <fornikaras7@gmail.com>
2024-10-24 22:45:46 +0500

    feat: ai page add, add dialogues

---
+++ src/features/ai/pages/components/chat/Ai-chat.jsx
@@ -0,0 +1,10 @@
+export const AiChat = () => {
+  return (
+    <div className="chat">
+      <div className="chat__window"></div>
+      <div>
+        <input type="text" className="chat__input" />
+      </div>
+    </div>
+  );
+};
